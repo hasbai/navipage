d429ddda2247c6f3d9023afe3534faa36c6f9301
shiyue2002 <jsclndnz@outlook.com>
2021-04-11 22:41:11 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app color="green">
+    <v-main>
+      <Home/>
+    </v-main>
+    <v-footer>
+      <v-col class="text-center" cols="12">
+        &copy; 2020-{{ new Date().getFullYear() }} All Rights Reserved
+      </v-col>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './views/Home.vue';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    Home,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,79 @@
+<template>
+<div>
+  <!-- <canvas class="fireworks"></canvas> -->
+  <v-img src="img/background.jpg" height="540">
+    <div style="margin-top:200px;" class="text-center text-h4 white--text">{{quote}}</div>
+  </v-img>
+  
+  <!-- <div style="margin-top:-180px;">{{quote}}</div> -->
+  <v-card class="py-16 mb-16" :class="smAndUp ? 'px-8 mx-12' : 'px-4 mx-4'" elevation="8" style="margin-top:-80px;">
+    <v-container fluid>
+      <v-row justify="center" v-for="(type, index) in config.types" :key="index">
+        <v-col cols="12" sm="10" md="6" lg="4"
+          v-for="(site, index) in type.sites" :key="index">
+          <v-hover v-slot="{ hover }">
+            <v-card @click="open(site.link)" :elevation="hover ? 16 : 2">
+              <div class="d-flex justify-start align-center py-4">
+                <v-img :src="site.img" height="75" max-width="100" contain :class="smAndUp ? 'mx-8' : 'mx-4'"></v-img>
+                <div>
+                  <v-card-title class="headline" v-text="site.name"></v-card-title>
+                  <v-card-subtitle v-text="site.description"></v-card-subtitle>
+                </div>
+              </div>
+            </v-card>
+          </v-hover>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>  
+</div>
+</template>
+
+<script>
+import anime from 'animejs'
+export default {
+  name: 'Home',
+  data(){
+    return {
+      config: {},
+      quote: '',
+    }
+  },
+  methods: {
+    open(url){
+      window.open(url)
+    },
+    getQuote(){
+      this.$axios.get('https://v1.hitokoto.cn/?c=i')
+      .then(res => {
+        console.log('get')
+          this.quote = res.data.hitokoto + '————《' + res.data.from + '》'
+        })
+      .catch(() => {
+        console.log('not get')
+      })
+    }
+  },
+  computed: {
+    smAndUp(){
+      return this.$vuetify.breakpoint.smAndUp
+    }
+  },
+  created(){
+    this.$axios.get('config.json').then(
+      res => {
+        this.config = res.data
+      }
+    )
+    // window.addEventListener('onload', this.getQuote)
+    this.getQuote()
+  },
+  
+}
+</script>
+<style scoped>
+  .fireworks {
+    /* position: absolute; */
+    /* z-index:-2; */
+  }
+</style>
